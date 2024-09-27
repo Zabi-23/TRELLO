f4fce0927be7de65ab888c23a369e8bb2a06298a
Zabi Sideqi <148572540+Zabi-23@users.noreply.github.com>
2024-09-27 21:44:24 +0200

    sista koden

--- dist/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// middleware/errorHandler.js
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || 500).json({ message: err.message });
-};
-exports.default = errorHandler;
--- dist/routes/exampelRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// src/routes/exampleRoutes.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/example', (req, res) => {
-    res.status(200).json({ message: 'Hello, World!' });
-});
-exports.default = router;
